0d10cf37639aaf394a358a3d6932028d39858d9d
Diane Leigh <leighd2008@gmail.com>
2023-02-05 12:38:47 -0500

    convert a single video working

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
@@ -21,12 +22,31 @@ app.on('ready', () => {
 });
 
 ipcMain.on('videos:added', (event, videos) => {
-  const promise = new Promise((resolve, reject) => {
-    ffmpeg.ffprobe(videos[0].path, (err, metadata) => {
-      resolve(metadata);
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        video.duration = metadata.format.duration;
+        video.format = 'avi';
+        resolve(video);
+      });
     });
   });
-  promise.then((metadata) => {console.log(metadata); })
+  Promise.all(promises)
+  .then((results) => {
+    mainWindow.webContents.send('metadata:complete', results);
+  });
+});
+
+ipcMain.on('conversion:start', (event, videos) => {
+  const video = videos[0];
+  
+  const outputDirectory = video.path.split(video.name)[0];
+  const outputName = video.name.split('.')[0]
+  const outputPath = `${outputDirectory}${outputName}.${video.format}`;
+  ffmpeg(video.path)
+    .output(outputPath)
+    .on('end', () => console.log('Video conversion complete.'))
+    .run();
 });
 
 module.exports = mainWindow;
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,14 @@
 import { ipcRenderer } from "electron";
 import { ADD_VIDEO, ADD_VIDEOS, REMOVE_VIDEO, REMOVE_ALL_VIDEOS, VIDEO_PROGRESS, VIDEO_COMPLETE } from "./types";
+import _ from "lodash";
 
 // TODO: Communicate to MainWindow process that videos
 // have been added and are pending conversion
 export const addVideos = videos => dispatch => {
   ipcRenderer.send('videos:added', videos);
+  ipcRenderer.on('metadata:complete', (event, videosWithData) => {
+    dispatch({ type: ADD_VIDEOS, payload: videosWithData });
+  });
 };
 
 
@@ -13,7 +17,8 @@ export const addVideos = videos => dispatch => {
 // from the MainWindow regarding the current state of
 // conversion.
 export const convertVideos = () => (dispatch, getState) => {
-
+  const videos = _.map(getState().videos);
+  ipcRenderer.send('conversion:start', videos);
 };
 
 // TODO: Open the folder that the newly created video
